1f9032f2521d1e5b244887cff9278e3edf97e947
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2024-07-22 08:41:04 +0700

    Kas Kelas v.1.0 Backend

---
+++ BE/src/main/java/com/kas_kelas/kas_kelas/KasKelasApplication.java
@@ -0,0 +1,13 @@
+package com.kas_kelas.kas_kelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasKelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasKelasApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/kas_kelas/kas_kelas/KasKelasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kas_kelas.kas_kelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasKelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
