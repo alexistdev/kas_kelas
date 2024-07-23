cfe269985e72096fe5c64cf6fa273deb15351c90
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-07-23 21:18:39 +0700

    feat(adminlte-template):integrate adminlte dashboard

--- FE/kas_kelas/src/App.js
+++ FE/kas_kelas/src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
 import Register from './components/Register';
+import Header from './components/Header';
+import SideNav from './components/SideNav';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="container mt-5">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
+        <Route 
+          path="/"
+          element={
+            <div>
+              <Header />
+              <SideNav />
+              <Home />
+              <Footer />
+            </div>
+          }
+          className="wrapper"
+
+        />
+      </Routes>
+      </BrowserRouter>
+
+
+
   );
 }
 
